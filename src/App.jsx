79f56a41cb68823883b9fc0dded41fd343911729
Aditi Singh <aditis@princeton.edu>
2025-07-27 18:35:43 -0400

    change base name again but in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import FundingAndAwards from './pages/FundingAndAwards.jsx'
 
 function App() {
   return (
-    <BrowserRouter basename="/aditisinghs.github.io">
+    <BrowserRouter basename="/">
 <nav className="fixed top-0 left-0 right-0 flex justify-end gap-6 py-6 px-4 bg-blue-50 shadow z-50">
         <Link to="/"   className="text-black font-medium text-lg px-4 py-1 border border-black rounded hover:bg-gray-200 transition-colors"
 >Home</Link>
